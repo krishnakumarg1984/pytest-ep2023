7b82c0bd8b36009db71efd9c840882f5d494aba3
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-10-02 20:18:35 +0100

    initial commit

--- code/01_basic/03_test_traceback.py
+++ code/01_basic/03_test_traceback.py
@@ -2,9 +2,11 @@
 
 from rpncalc.utils import calc
 
+
 def test_divide():
     # This will raise ZeroDivisionError
-    assert calc(2, 0, '/') == 0
+    assert calc(2, 0, "/") == 0
+
 
 def test_good():
     pass
--- code/01_basic/04_test_raises.py
+++ code/01_basic/04_test_raises.py
@@ -1,9 +1,7 @@
+import pytest
 from rpncalc.utils import calc
 
-import pytest
-
-# basic/test_raises.py
 
 def test_zero_division():
     with pytest.raises(ZeroDivisionError):
-        calc(3, 0, '/')
+        calc(3, 0, "/")
